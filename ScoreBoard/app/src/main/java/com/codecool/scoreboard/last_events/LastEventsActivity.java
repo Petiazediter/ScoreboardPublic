6e745ab083243b481b7cfe90687c14e59a27082c
Bogi Kiss <bogikiss85@gmail.com>
2020-06-22 18:06:59 +0200

    init activities for events

---
+++ ScoreBoard/app/src/main/java/com/codecool/scoreboard/last_events/LastEventsActivity.java
@@ -0,0 +1,18 @@
+package com.codecool.scoreboard.last_events;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codecool.scoreboard.R;
+
+public class LastEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last_events);
+
+        
+    }
+}
