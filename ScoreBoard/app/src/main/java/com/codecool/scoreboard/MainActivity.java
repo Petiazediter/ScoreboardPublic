239a1c0293dbda3981d71774a4e7a8b2fb904d51
Petiazediter <pverebes74@gmail.com>
2020-06-22 14:08:41 +0200

    created viewpager

--- ScoreBoard/app/src/main/java/com/codecool/scoreboard/MainActivity.java
+++ ScoreBoard/app/src/main/java/com/codecool/scoreboard/MainActivity.java
@@ -1,14 +1,23 @@
 package com.codecool.scoreboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_view_pager)
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
