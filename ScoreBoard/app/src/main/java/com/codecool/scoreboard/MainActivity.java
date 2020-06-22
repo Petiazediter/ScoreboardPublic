58f0fe5c304e6282ab42640aaed6885231c29dc3
Petiazediter <pverebes74@gmail.com>
2020-06-22 14:26:03 +0200

    ViewPager with 2 basic fragment

--- ScoreBoard/app/src/main/java/com/codecool/scoreboard/MainActivity.java
+++ ScoreBoard/app/src/main/java/com/codecool/scoreboard/MainActivity.java
@@ -1,9 +1,18 @@
 package com.codecool.scoreboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.codecool.scoreboard.fragments.Fragment1;
+import com.codecool.scoreboard.fragments.Fragment2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_view_pager)
     ViewPager viewPager;
+    PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new Fragment1());
+        fragmentList.add(new Fragment2());
+
+        pagerAdapter = new SlideAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, fragmentList);
+        viewPager.setAdapter(pagerAdapter);
     }
+
+
 }
